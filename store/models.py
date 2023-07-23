5028672afef5f2d27066e4d5ddf5aebf03364579
Gervas Germinus <gervasgerminus@gmail.com>
2023-07-23 23:33:17 +0300

    V2 Model

--- store/models.py
+++ store/models.py
@@ -10,17 +10,11 @@ class User(models.Model):
     recoveryToken = models.CharField(max_length=100)
 
 
-class Item(models.Model):
-    sku = models.CharField(max_length=100,primary_key=True)
-    item_description = models.CharField(max_length=255)
-    category = models.CharField(max_length=255)
-    uom = models.DecimalField(max_digits=5, decimal_places=2)
-    reorder_point = models.BigIntegerField()
 
 class Purchase(models.Model):
+    sku = models.CharField(max_length=100, primary_key=True)
     purchase_order = models.CharField(max_length=100,primary_key=True)
     purchase_date = models.DateField(auto_now_add=True)
-    sku = models.ForeignKey(Item,on_delete=models.CASCADE)
     supplier = models.CharField(max_length=255)
     quantity = models.BigIntegerField()
     discount = models.FloatField()
@@ -28,17 +22,23 @@ class Purchase(models.Model):
     total = models.DecimalField(max_digits=5, decimal_places=2)
 
 class inventory(models.Model):
-    sku = models.ForeignKey(Item,on_delete=models.CASCADE)
+    sku = models.ForeignKey(Purchase,on_delete=models.CASCADE)
+    item_name = models.CharField(max_length=255)
+    item_description = models.CharField(max_length=255)
+    category = models.CharField(max_length=255)
+    uom = models.DecimalField(max_digits=5, decimal_places=2)
+    reorder_point = models.BigIntegerField()
     total_purchases = models.DecimalField(max_digits=6,decimal_places=2)
     total_sales = models.DecimalField(max_digits=6,decimal_places=2)
     stock_available = models.BigIntegerField()
     a_s_value = models.DecimalField(max_digits=5, decimal_places=2)
     best_selling_product = models.CharField(max_length=255)
 
+
 class Sale(models.Model):
     sale_order = models.CharField(max_length=100,primary_key=True)
     sale_date = models.DateField(auto_now_add=True)
-    sku = models.ForeignKey(Item,on_delete=models.CASCADE)
+    sku = models.ForeignKey(,on_delete=models.CASCADE)
     quantity = models.BigIntegerField()
     discount = models.FloatField()
     unit_price = models.DecimalField(max_digits=5, decimal_places=2)
