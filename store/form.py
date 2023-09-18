b99bea88b4302046f61be72c152d27d100f343d7
Gervas Germinus <gervasgerminus@gmail.com>
2023-09-18 23:18:42 +0300

    Fix Purchase and Inventory Form

--- store/form.py
+++ store/form.py
@@ -1,26 +1,14 @@
+from django.forms import ModelForm
 from django import forms
 from .models import *
 
 
-
-'''
- purchase_fields = {
-                'sku': purchase_form.cleaned_data['sku'],
-                'purchase_order': purchase_form.cleaned_data['purchase_order'],
-                'purchase_date': purchase_form.cleaned_data['purchase_date'],
-                'supplier': purchase_form.cleaned_data['supplier'],
-                'quantity': purchase_form.cleaned_data['quantity'],
-                'discount': purchase_form.cleaned_data['discount'],
-                'unit_price': purchase_form.cleaned_data['unit_price'],
-                'total': purchase_form.cleaned_data['total'],
-                'expire_date': purchase_form.cleaned_data['expire_date'],
-            }
-'''
-class PurchaseForm(forms.ModelForm):
+class PurchaseForm(ModelForm):
     class Meta:
         model = Purchase
-        fields = '__all__'
+        exclude = ['purchase_date']
         widgets = {
+            'sku': forms.TextInput(attrs={'id': 'sku_id'}),
             'quantity': forms.NumberInput(attrs={'id': 'quantity'}),
             'discount': forms.NumberInput(attrs={'id': 'discount', 'step': '.01'}),
             'unit_price': forms.NumberInput(attrs={'id': 'unit_price', 'step': '.01'}),
@@ -28,7 +16,7 @@ class PurchaseForm(forms.ModelForm):
             'expire_date': forms.DateInput(attrs={'id': 'expire_date', 'type': 'date'}),
         }
 
-class InventoryForm(forms.ModelForm):
+class InventoryForm(ModelForm):
     class Meta:
         model = Inventory
         exclude = ['total_sales','best_selling_product']
@@ -37,23 +25,24 @@ class ReservedForm(forms.ModelForm):
         model = Reserved
         fields = '__all__'
 
-class SaleForm(forms.ModelForm):
+class SaleForm(ModelForm):
     class Meta:
         model = Sale
         exclude = ['sale_date']
         widgets = {
+            'sku': forms.TextInput(attrs={'id': 'inv_sku_id'}),
             'quantity': forms.NumberInput(attrs={'id': 'quantity'}),
             'discount': forms.NumberInput(attrs={'id': 'discount', 'step': '.01'}),
             'unit_price': forms.NumberInput(attrs={'id': 'unit_price', 'step': '.01'}),
             'total_amount': forms.NumberInput(attrs={'id': 'total_amount', 'step': '.01'}),
         }
 
-class CustomerForm(forms.ModelForm):
+class CustomerForm(ModelForm):
     class Meta:
         model = Customer
         fields = '__all__'
 
-class UserForm(forms.ModelForm):
+class UserForm(ModelForm):
     class Meta:
         model = User
         fields = '__all__'
--- store/store_views/purchase.py
+++ store/store_views/purchase.py
@@ -1,52 +1,25 @@
 from django.views import View
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from store.models import Purchase, Inventory
 from store.form import PurchaseForm, InventoryForm
 
 
 class PurchaseBoard(View):
-
     template_name = 'purchases.html'
-
     def get(self, request):
         purchase_form = PurchaseForm()
         inventory_form = InventoryForm()
-        return render(request, self.template_name, {'purchase_form':purchase_form,'inventory_form': inventory_form})
-
+        return render(request, self.template_name,{'purchase_form': purchase_form,'inventory_form':inventory_form})
     def post(self, request):
         purchase_form = PurchaseForm(request.POST)
         inventory_form = InventoryForm(request.POST)
-
-        if purchase_form.is_valid() and inventory_form.is_valid():
-            purchase_fields = {
-                'sku': purchase_form.cleaned_data['sku'],
-                'purchase_order': purchase_form.cleaned_data['purchase_order'],
-                'purchase_date': purchase_form.cleaned_data['purchase_date'],
-                'supplier': purchase_form.cleaned_data['supplier'],
-                'quantity': purchase_form.cleaned_data['quantity'],
-                'discount': purchase_form.cleaned_data['discount'],
-                'unit_price': purchase_form.cleaned_data['unit_price'],
-                'total': purchase_form.cleaned_data['total'],
-                'expire_date': purchase_form.cleaned_data['expire_date'],
-            }
-            inventory_fields = {
-                'sku': inventory_form.cleaned_data['sku'],
-                'item_name': inventory_form.cleaned_data['item_name'],
-                'item_description': inventory_form.cleaned_data['item_description'],
-                'category': inventory_form.cleaned_data['category'],
-                'uom': inventory_form.cleaned_data['uom'],
-                'reorder_point': inventory_form.cleaned_data['reorder_point'],
-                'total_purchases': inventory_form.cleaned_data['total'],
-                'stock_available_main': inventory_form.cleaned_data['stock_available_main'],
-                'stock_available_for_sale': inventory_form.cleaned_data['stock_available_main'],
-                'stock_available_value': purchase_form.cleaned_data['quality'] * purchase_form.cleaned_data['unit_price'] if (inventory_form.cleaned_data['stock_available_main'] == 0) else purchase_form.cleaned_data['unit_price'] * inventory_form.cleaned_data['stock_available_main'],
-
-            }
-            Purchase.objects.create(**purchase_fields)
-            Inventory.objects.create(**inventory_fields)
-            Purchase.save()
-            Inventory.save()
+        if(purchase_form.is_valid()):
+            purchase_instance = purchase_form.save(commit=False)
+            if inventory_form.is_valid():
+                inventory_instance = inventory_form.save(commit=False)
+                inventory_instance.sku = purchase_instance.sku
+                inventory_instance.save()
+            purchase_instance.save()
             return redirect('purchases')
-
-        return render(request, self.template_name, {'purchase_form':purchase_form,'inventory_form':inventory_form})
+        return render(request, self.template_name, {'purchase_form': purchase_form, 'inventory_form': inventory_form })
 
